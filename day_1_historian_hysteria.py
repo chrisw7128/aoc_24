d24006915b0a10e66427bde519db5b101c11b41f
Chris Wehner <chrisw7128@gmail.com>
2024-12-07 11:38:28 -0800

    added day 1 part 2/2

--- day_1_historian_hysteria.py
+++ day_1_historian_hysteria.py
@@ -1,6 +1,8 @@
 with open('day_1_input.txt') as fin:
     myfile = fin.readlines()
 
+# Part 1/2
+
 all_coords = [pair.split() for pair in myfile]
 [first_list] = [sorted(int(coord[0]) for coord in all_coords)]
 [second_list] = [sorted(int(coord[1]) for coord in all_coords)]
@@ -15,3 +17,19 @@ def find_total_distance(list1, list2):
 
 total_distance = find_total_distance(first_list, second_list)
 print(f'The total distance is {total_distance}')
+
+# Part 2/2
+
+from collections import Counter
+
+count_in_second_list = Counter(second_list)
+
+def find_similarity_score(list1, counter1):
+    similarity_score = 0
+    for coord in list1:
+        if counter1[coord]:
+            similarity_score += coord*counter1[coord]
+    return similarity_score
+
+similarity_score = find_similarity_score(first_list, count_in_second_list)
+print(f'The similarity score is {similarity_score}')
